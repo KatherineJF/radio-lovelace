170fea1f9a1f595ba7d58106d20d06336f92b918
KatherineJF <kjfitzpat@hotmail.com>
2018-12-10 22:41:17 -0800

    convert RadioSet to class

--- src/components/RadioSet.js
+++ src/components/RadioSet.js
@@ -3,23 +3,28 @@ import "./styles/RadioSet.css";
 
 import Playlist from './Playlist';
 
-const RadioSet = (props) => {
-  console.log(`Radio set for ${props.tracks.length} tracks`);
-  const playlists = {
-    morningTracks: props.tracks.slice(0, props.tracks.length / 2),
-    eveningTracks: props.tracks.slice(props.tracks.length / 2, props.tracks.length)
-  };
+class RadioSet extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      morningTracks: props.tracks.slice(0, props.tracks.length / 2),
+      eveningTracks: props.tracks.slice(props.tracks.length / 2, props.tracks.length)
+    }
+
+  }
+
   return (
     <div className="radio-set">
       <section className="radio-set--playlist-container">
+      <Playlist
+         side="Morning"
+         tracks={this.state.morningTracks}
+       />
         <Playlist
-          side="Morning"
-          tracks={playlists.morningTracks}
-        />
-        <Playlist
-          side="Evening"
-          tracks={playlists.eveningTracks}
-        />
+           side="Evening"
+           tracks={this.state.eveningTracks}
+         />
       </section>
     </div>
   );
